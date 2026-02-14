433bab393de820cb424ed661fe64c8edaaeaa6f2
Weibo0123 <weibolin322@gmail.com>
2026-02-14 11:42:01 -0600

    add the first experiments

---
+++ experiments/alpha_finder/alpha_comparison.py
@@ -0,0 +1,9 @@
+"""
+alpha_comparison.py
+"""
+
+from src.model import predict, gradient_descent
+
+
+
+
---
+++ src/model.py
@@ -0,0 +1,28 @@
+"""
+model.py
+"""
+import numpy as np
+
+def predict(x: np.ndarray, w: float, b: float) -> np.ndarray:
+    return x * w + b
+
+def compute_cost(x: np.ndarray, y: np.ndarray, w: float, b: float) -> float:
+    m = len(x)
+    y_hat = predict(x, w, b)
+    error = y_hat - y
+    return (1 / (2 * m)) * np.sum(error ** 2)
+
+def gradient_descent(x: np.ndarray, y: np.ndarray, w: float, b: float, alpha: float, num_iters: int) -> tuple [float, float, list]:
+    m = len(x)
+    cost_history = []
+    for i in range(num_iters):
+        y_hat = predict(x, w, b)
+        error = y_hat - y
+        dw = (1 / m) * np.sum(x * error)
+        db = (1 / m) * np.sum(error)
+        w -= alpha * dw
+        b -= alpha * db
+
+        if i % 10 == 0:
+            cost_history.append(compute_cost(x, y, w, b))
+    return w, b, cost_history
